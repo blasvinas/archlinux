96d69038ac77695362248234e647edfb6fdb5996
Blas Vinas <blasvinas@gmail.com>
2025-02-13 19:59:50 -0500

    modify install.sh

--- install.sh
+++ install.sh
@@ -1,41 +1,41 @@
-paru -R dolphin
-paru -S neovim
-paru -S thunderbird
-paru -S libreoffice-fresh
-paru -S fish
-paru -S fastfetch
-paru -S starship
-paru -S 1password
-paru -S nautilus
-paru -S dropbox
-paru -S nautilus-dropbox
-paru -S pavucontrol
-paru -S unzip
-paru -S blueman
-paru -S ttf-meslo-nerd
-paru -S ttf-font-awesome
-paru -S hyprlock
-paru -S hypridle
-paru -S brightnessctl
-paru –S simple-scan
-paru -S sane-airscan
-paru -S dracula-gtk-theme
-paru -S candy-icons
-paru -S polkit-gnome
-paru -S impression
-paru -S sddm-theme-sugar-candy-git
-paru -S rclone
-paru -S timeshift
-paru -S deja-dup
-paru -S python-requests-oauthlib
-paru -S gnome-keyring
-paru -S --needed wireplumber libgtop bluez bluez-utils btop networkmanager dart-sass wl-clipboard brightnessctl swww python upower pacman-contrib power-profiles-daemon gvfs
-paru -S --needed aylurs-gtk-shell-git grimblast-git gpu-screen-recorder-git hyprpicker matugen-bin python-gpustat hyprsunset-git hypridle-git
-paru -S ags-hyprpanel-git
+paru -R --noconfirm dolphin
+paru -S --needed --noconfirm neovim
+paru -S --needed --noconfirm thunderbird
+paru -S --needed --noconfirm libreoffice-fresh
+paru -S --needed --noconfirm fish
+paru -S --needed --noconfirm fastfetch
+paru -S --needed --noconfirm starship
+paru -S --needed --noconfirm 1password
+paru -S --needed --noconfirm nautilus
+paru -S --needed --noconfirm dropbox
+paru -S --needed --noconfirm nautilus-dropbox
+paru -S --needed --noconfirm pavucontrol
+paru -S --needed --noconfirm unzip
+paru -S --needed --noconfirm blueman
+paru -S --needed --noconfirm ttf-meslo-nerd
+paru -S --needed --noconfirm ttf-font-awesome
+paru -S --needed --noconfirm hyprlock
+paru -S --needed --noconfirm hypridle
+paru -S --needed --noconfirm brightnessctl
+paru –S --needed --noconfirm simple-scan
+paru -S --needed --noconfirm sane-airscan
+paru -S --needed --noconfirm dracula-gtk-theme
+paru -S --needed --noconfirm candy-icons
+paru -S --needed --noconfirm polkit-gnome
+paru -S --needed --noconfirm impression
+paru -S --needed --noconfirm sddm-theme-sugar-candy-git
+paru -S --needed --noconfirm rclone
+paru -S --needed --noconfirm timeshift
+paru -S --needed --noconfirm deja-dup
+paru -S --needed --noconfirm python-requests-oauthlib
+paru -S --needed --noconfirm gnome-keyring
+paru -S --needed --noconfirm wireplumber libgtop bluez bluez-utils btop networkmanager dart-sass wl-clipboard brightnessctl swww python upower pacman-contrib power-profiles-daemon gvfs
+paru -S --needed --noconfirm aylurs-gtk-shell-git grimblast-git gpu-screen-recorder-git hyprpicker matugen-bin python-gpustat hyprsunset-git hypridle-git
+paru -S --needed --noconfirm ags-hyprpanel-git
 gsettings set org.gnome.desktop.interface gtk-theme Dracula
 gsettings set org.gnome.desktop.interface icon-theme candy-icons
 gsettings set org.gnome.desktop.interface font-name "MesloLGS Nerd Font Mono"
 git config --global user.email "blasvinas@gmail.com"
 git config --global user.name "Blas Vinas"
-ssh-keygen
 paru -c
+ssh-keygen
