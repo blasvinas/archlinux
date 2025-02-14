ae8ae1fa2060f23c32b7d7e32bc4abbb019e85ab
Blas Vinas <blasvinas@gmail.com>
2025-02-13 20:09:55 -0500

    delete non needed files

--- backup.sh
+++
@@ -1,3 +0,0 @@
-notify-send "Starting backup"
-rclone sync /home/blas/Dropbox/ /home/blas/OneDrive/   --exclude ".dropbox.cache/**"
-notify-send "Backup completed"
--- onedrive.sh
+++
@@ -1,6 +0,0 @@
-rclone --vfs-cache-mode writes mount OneDrive:/Linux ~/OneDrive --daemon
-notify-send "OneDrive connected" 
-sleep 3
-notify-send "Starting backup"
-rclone sync /home/blas/Dropbox/ /home/blas/OneDrive/   --exclude ".dropbox.cache/**"
-notify-send "Backup completed"
