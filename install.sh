19d9b8d7ebe875c62998f99c3415c096a6b7016e
Blas Vinas <blasvinas@gmail.com>
2025-02-13 19:42:16 -0500

    add install.sh

---
+++ install.sh
@@ -0,0 +1,36 @@
+paru -R dolphin
+paru -S neovim
+paru -S thunderbird
+paru -S libreoffice-fresh
+paru -S fish
+paru -S fastfetch
+paru -S starship
+paru -S 1password
+paru -S nautilus
+paru -S dropbox
+paru -S nautilus-dropbox
+paru -S pavucontrol
+paru -S wlogout
+paru -S unzip
+paru -S nwg-drawer
+paru -S blueman
+paru -S ttf-meslo-nerd
+paru -S ttf-font-awesome
+paru -S hyprlock
+paru -S hypridle
+paru -S brightnessctl
+paru –S simple-scan
+paru -S sane-airscan
+paru -S dracula-gtk-theme
+paru -S candy-icons
+paru -S polkit-gnome
+paru -S impression
+paru -S sddm-theme-sugar-candy-git
+paru -S rclone
+paru -S timeshift
+paru -S deja-dup
+paru -S python-requests-oauthlib
+paru -S gnome-keyring
+gsettings set org.gnome.desktop.interface gtk-theme Dracula
+gsettings set org.gnome.desktop.interface icon-theme candy-icons
+gsettings set org.gnome.desktop.interface font-name "MesloLGS Nerd Font Mono"
